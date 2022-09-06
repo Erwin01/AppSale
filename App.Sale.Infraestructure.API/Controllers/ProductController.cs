6b91acd09659838b9e2fcaba45c6015a5b7a99c6
Erwin <erwin.paralesda@gmail.com>
2022-09-06 13:32:05 -0500

    Add files via upload

---
+++ App.Sale.Infraestructure.API/Controllers/ProductController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using App.Sale.Domain.Interfaces.Repositories;
+
+using App.Sale.Domain;
+using App.Sale.Application.Services;
+using App.Sale.Infraestructure.Data.Repositories;
+using App.Sale.Infraestructure.Data.Contexts;
+
+namespace App.Sale.Infraestructure.API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        #region [ CREATE SERVICE ]
+        static ProductService CreateService()
+        {
+            SaleContext _db = new SaleContext();
+            ProductRepository repository = new ProductRepository(_db);
+            ProductService service = new ProductService(repository);
+
+            return service;
+        }
+        #endregion
+
+
+        // GET: api/<ProductController>
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetAsync()
+        {
+
+            var service = CreateService();
+
+            return Ok(await service.Lists());
+        }
+
+
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetByIdAsync(Guid id)
+        {
+
+            var service = CreateService();
+
+            return Ok(await service.SelectByID(id));
+        }
+
+
+
+        // POST api/<ProductController>
+        [HttpPost]
+        public async Task<ActionResult> PostAsync([FromBody] Product product)
+        {
+
+            var service = CreateService();
+            await service.Add(product);
+
+            return Ok("Successfully!");
+        }
+
+
+
+        // PUT api/<ProductController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutAsync(Guid id, [FromBody] Product product)
+        {
+
+            var service = CreateService();
+            product.ProductId = id;
+            await service.Edit(product);
+
+            return Ok("Edited successfully!");
+
+        }
+
+
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteAsync(Guid id)
+        {
+
+            var service = CreateService();
+            await service.Delete(id);
+
+            return Ok("Deleted successfully!");
+        }
+    }
+}
---
+++ App.Sale.Infraestructure.API/Controllers/SaleController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using App.Sale.Domain;
+using App.Sale.Application.Services;
+using App.Sale.Infraestructure.Data.Repositories;
+using App.Sale.Infraestructure.Data.Contexts;
+
+namespace App.Sale.Infraestructure.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SaleController : ControllerBase
+    {
+
+        #region [ CREATE SERVICE ]
+        static SaleService CreateService()
+        {
+            SaleContext _db = new SaleContext();
+            SaleRepository repositorySale = new SaleRepository(_db);
+            ProductRepository productRepository = new ProductRepository(_db);
+            SaleDetailRepository saleDetailRepository = new SaleDetailRepository(_db);
+            SaleService service = new SaleService(repositorySale, productRepository, saleDetailRepository);
+
+            return service;
+        }
+        #endregion
+
+
+
+        // GET: api/<SaleController>
+        [HttpGet]
+        public async Task<ActionResult<List<Sales>>> GetAsync()
+        {
+            var service = CreateService();
+
+            return Ok(await service.Lists());
+
+        }
+
+
+
+        // GET api/<SaleController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Sales>> GetByIdAsync(Guid id)
+        {
+
+            var service = CreateService();
+
+            return Ok(await service.SelectByID(id));
+
+        }
+
+
+
+        // POST api/<SaleController>
+        [HttpPost]
+        public async Task<ActionResult> PostAsync([FromBody] Sales sales)
+        {
+
+            var service = CreateService();
+            await service.Add(sales);
+
+            return Ok("Save sale successfully!");
+
+        }
+
+
+        // No Exists Function By Edit
+
+
+
+        // DELETE api/<SaleController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteAsync(Guid id)
+        {
+
+            var service = CreateService();
+            await service.Cancel(id);
+
+            return Ok("Cancel sale successfully!");
+        }
+    }
+}
